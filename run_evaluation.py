f77ec4bc1b7c34a3c4b52e23eca77c8eca9cb9bf
Haodong Zhang <haodoz0118@gmail.com>
2026-01-23 16:38:22 -0500

    Fixed the bug

--- run_evaluation.py
+++ run_evaluation.py
@@ -10,7 +10,7 @@ from eval import evaluate_checkpoint
 # =============================================================================
 
 # Paths
-CHECKPOINT_PATH = "/root/best.ckpt"
+CHECKPOINT_PATH = "/root/results/ckpts/best.ckpt"
 DATA_FOLDER = "/root/ICML_2026_FIN_HUMPBACK_WHALE/RESOURCES/SEGLVIK"
 CONFIG_PATH = "configs/cpc_config.yaml"
 
